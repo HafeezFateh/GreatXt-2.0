cb6cd6fd01ddb5f51b9b05fc281fa27d45021d00
Hafeezur Rahman <hafeezofficial08@gmail.com>
2025-08-09 22:51:29 +0530

    V.1.1.1

---
+++ src/Home.jsx
@@ -0,0 +1,33 @@
+import Header from "./components/Header";
+
+
+function Home(){
+
+
+
+    return(
+        <>
+            <Header />
+
+            <div id="hero" className="mx-5 flex flex-col items-center mt-25">
+                <h1 className="p-2 bricolage-grotesque text-5xl lg:text-8xl bg-clip-text bg-gradient-to-br from-stone-100 to-stone-600 text-transparent">GreatXt Agency</h1>
+                <p className="text-3xl poppins-regular  lg:text-4xl text-stone-300 text-center">Your <span className="text-lime-500">Brand's success</span>, <br /> is what we want.</p>
+            </div>
+            
+            <div className="flex justify-center mt-20 p-5">
+                <img src="./greatxt.png" alt="" className="w-[200px] lg:w-[400px] opacity-5"/>
+            </div>
+
+            <div className="flex justify-center">
+            <div className="w-[1000px] h-[200px] blur-3xl opacity-5 bg-lime-500 rounded-full"></div>
+            </div>
+
+             
+         
+            
+            
+        </>
+    )
+}
+
+export default Home;
---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+function Header(){
+
+    const [open, setOpen] = useState(false);
+
+    return(
+        <>
+               <div id="header" className="flex items-center justify-between p-5 mt-2 lg:mx-20">
+
+                <div className="flex gap-2">
+                    <img src="./greatxt.png" className="" alt= "" />
+                </div>
+
+                <span onClick={() => setOpen (prev => !prev) } className="material-symbols-outlined text-stone-50"><span className="lg:hidden">density_medium</span> </span>
+
+                <div className="hidden lg:flex gap-5 text-stone-200 text-lg">  
+                               
+                    <Link to='/' className="py-1 poppins-regular hover:text-lime-600">About</Link>
+                    <Link to='/' className="py-1 poppins-regular hover:text-lime-600">Serices</Link>
+                    <Link to='/' className="px-5 py-1  bg-lime-600  rounded-4xl  text-stone-900 shadow-xl shadow-lime-300/10  poppins-regular hover:text-lime-100 ">Contact</Link>
+                    
+                </div>
+
+
+            </div>
+
+            { open &&
+             <div className="text-3xl text-stone-200 flex flex-col border-b  border-lime-600 p-2  items-center lg:hidden ">
+                <Link to='/' className="p-3 hover:text-lime-600">About</Link>
+                <Link to='/' className="p-3 hover:text-lime-600">Serices</Link>
+                <Link to='/' className="p-3 hover:text-lime-600 pb-7">Contact</Link>
+            </div>
+            }
+        
+        </>
+    )
+}
+
+export default Header;
